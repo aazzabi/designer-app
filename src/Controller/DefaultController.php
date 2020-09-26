8860d43b107636d7fe2d42ce9d15ef6987213baf
Arafet <arafet.azzabi@esprit.tn>
2020-09-26 23:45:29 +0100

    Registration + Email confirmation

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controller;
+
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Security\EmailVerifier;
+use App\Security\LoginFormAuthenticator;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function index() {
+        return $this->render('index.html.twig', []);
+    }
+
+}
